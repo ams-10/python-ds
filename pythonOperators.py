4af0e222cb30e7bb2d32624172a091e4a1551f62
ams-10 <abdullahshareef123h@gmail.com>
2023-11-06 12:21:51 +0530

    python operators

--- pythonOperators.py
+++ pythonOperators.py
@@ -2,13 +2,14 @@
 #operand the symbol on which the task is performed
 '''
 operators:  arithmetic {+=>ADDITION; SUBRACTION=> -; MULTIPLICATION => *;DIVISION => /; MODULO => % (gives remainder); POWER => **; FLOORDIVISION => //}
-            assignment { }
-            comparison/conditional 
-            logical 
-            bitwise    '''
+            assignment { = += -= }
+            comparison/conditional {== >= <= !=}
+            logical  
+            bitwise    
+'''
 #FLOOR DIVISION neglects float values and gives approximate values
 #NORMAL DIVISION give accurate values
 print(7.0//2)
 print(7//2)
 
-print()
+#in used to check if an element is present in a sequence data type it returns true or false
