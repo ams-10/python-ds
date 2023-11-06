0e361f0524452edcb41e8d772125f9bada374abf
ams-10 <abdullahshareef123h@gmail.com>
2023-11-06 10:39:05 +0530

    python basics

---
+++ pythonDatatypes.py
@@ -0,0 +1,15 @@
+list1 = [2,4,8,10,12,14,16,18] #enclosed in square brackets mutable ordered collection of items
+print(list1, type(list1))
+print('\n')
+
+tuple1 = ('a', "b" , "c") #ordered collection of immutable objects but can be changed by converting into list
+tuple2 = (1,) #if comma not placed the type function will return integer
+print(tuple1,type(tuple1),"\n",tuple2,type(tuple2))
+print("\n")
+
+tup = (15, [15,12,"python"])
+print(type(tup))
+print(tup[1][-1])
+tup[1][-1] = "python programming"
+print(tup)
+print('\n')
