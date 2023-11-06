6de6913d502c2aae2975914565cbe3bb09c8a0a6
ams-10 <abdullahshareef123h@gmail.com>
2023-11-06 12:01:14 +0530

    python operators

--- pythonOperators.py
+++ pythonOperators.py
@@ -1,1 +1,12 @@
-pythonOperators.py
+#operator is a special symbol which performs a specific task
+#operand the symbol on which the task is performed
+'''
+operators:  arithmetic {+=>ADDITION; SUBRACTION=> -; MULTIPLICATION => *;DIVISION => /; MODULO => % POWER => **; FLOORDIVISION => //}
+            assignment { }
+            comparison/conditional 
+            logical 
+            bitwise    '''
+#FLOOR DIVISION neglects float values and gives approximate values
+#NORMAL DIVISION give accurate values
+print(7.0//2)
+print(7//2)
