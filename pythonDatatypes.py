dab90f1d06efa3958a8e7349fb28ced2dcf42599
ams-10 <abdullahshareef123h@gmail.com>
2023-11-06 10:55:16 +0530

    python datatypes

--- pythonDatatypes.py
+++ pythonDatatypes.py
@@ -1,19 +1,10 @@
-list1 = [2,4,8,10,12,14,16,18] #enclosed in square brackets mutable ordered collection of items
-print(list1, type(list1))
-print('\n')
+"""numerical int float complex"""
+"""sequence type {ordered list,tuple,string} unordered {set,dictionary}"""
+"""boolean true false"""
+#float precision == 50  
+#print commented part 
+def fun():
+    """IT LORDS"""
+    return print(fun.__doc__)
 
-tuple1 = ('a', "b" , "c") #ordered collection of immutable objects but can be changed by converting into list
-tuple2 = (1,) #if comma not placed the type function will return integer
-print(tuple1,type(tuple1),"\n",tuple2,type(tuple2))
-print("\n")
-
-tup = (15, [15,12,"python"])
-print(type(tup))
-print(tup[1][-1])
-tup[1][-1] = "python programming"
-print(tup)
-print('\n')
-
-print(chr(35646)) #get an ascii character
-print(ord("😂")) #get the ascii value of a character
-print('\n')
+#iskeyword() => the function is used to check if its key word or not 
